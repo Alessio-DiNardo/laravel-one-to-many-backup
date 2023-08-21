08a5bd1622ead0dba89a965e4bf9e3aff301749e
Alessio Di Nardo <127017838+Alessio-DiNardo@users.noreply.github.com>
2023-08-21 16:49:06 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+import './bootstrap';
+
+import * as bootstrap from 'bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+import.meta.glob([
+    '../img/**'
+])

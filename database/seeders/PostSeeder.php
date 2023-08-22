41cca0cce67a1ec91fc1f7728594da2408b19891
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-08-22 19:36:42 +0200

    add faker in seeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -4,14 +4,23 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class PostSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i=0; $i < 100; $i++) {
+            $newPost = new Post();
+            $newPost->title = ucfirst($faker->unique()->words(4, true));
+            $newPost->content = $faker->paragraphs(10, true);
+            $newPost->slug = $faker->imageUrl();
+            $newPost->image= $faker->imageUrl(480, 360, 'post', true, 'posts', true, 'png');
+            $newPost->save();
+        }
+
     }
 }

d4a8820484f2ce121a924044c2b170eccecc5921
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-08-24 16:26:53 +0200

    fix in post controller index and create

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -14,7 +14,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::paginate(15);
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
@@ -22,7 +23,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.posts.create');
     }
 
     /**

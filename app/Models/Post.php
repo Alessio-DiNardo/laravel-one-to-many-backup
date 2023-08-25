c3133c7c38b5064f3df8127c92c9d900b39a36c1
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-08-25 16:12:29 +0200

    add must contrroller, fix user and post models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+
+    protected $fillable = [
+        'title',
+        'content',
+        'image',
+        'slug'
+    ];
+
+    /**
+    * Get the route key for the model.
+    */
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
 }

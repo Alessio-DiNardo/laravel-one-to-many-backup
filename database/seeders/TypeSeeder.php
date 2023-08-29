e2c37747507f478060841d45289792ec44e670db
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-08-29 12:28:12 +0200

    Add migration and type seedeer

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        $types = [
+            'Art',
+            'History',
+            'Game',
+            'Horror',
+        ];
+        //
+        foreach ($types as $type) {
+            $newType = new Type();
+            $newType->name = $type;
+            $newType->color = $faker->hexColor();
+            $newType->save();
+        }    
+    }
+}
